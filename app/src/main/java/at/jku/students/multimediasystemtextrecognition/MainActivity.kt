e92f7dec3cd1c7ae6eba59c1368085e2f7454eec
Daniel Morawetz <daniel@morawetz.dev>
2023-03-24 15:45:03 +0100

    make hello world work

--- app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
+++ app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
@@ -2,21 +2,40 @@ package at.jku.students.multimediasystemtextrecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Displaying edge-to-edge
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        //WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            Text(
-                text = "Hello World!"
-            )
+            MaterialTheme {
+                Welcome()
+            }
+
         }
+    }
+}
+
+@Composable
+@Preview
+fun Welcome() {
+    Column {
+        Text(
+            text = "Hello World!",
+            color = Color.Black
+        )
     }
 }
