bd0e4b758fed935bdedadbb6912468295676a93b
Daniel Morawetz <daniel@morawetz.dev>
2023-05-23 19:19:15 +0200

    add ml kit text recognition with test image

--- app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
+++ app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
@@ -1,17 +1,26 @@
 package at.jku.students.multimediasystemtextrecognition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +41,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun Welcome() {
+    var detectedText by remember { mutableStateOf("") }
+
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    val img = ImageBitmap.imageResource(id = R.drawable.test)
+
+    val inp = InputImage.fromBitmap(img.asAndroidBitmap(), 0)
+
+    recognizer.process(inp)
+        .addOnSuccessListener {
+            detectedText = it.text
+        }
+        .addOnFailureListener{
+            print(it.message)
+        }
+
     Column {
         Text(
-            text = "Hello World!",
+            text = "Hello World! $detectedText",
             color = Color.Black
+        )
+        Image(
+            painter = painterResource(R.drawable.test),
+            contentDescription = ""
         )
     }
 }
