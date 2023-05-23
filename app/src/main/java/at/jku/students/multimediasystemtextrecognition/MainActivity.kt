ca3c136cbe4f31f09687eb96cee19ac1e72c021c
Daniel Morawetz <daniel@morawetz.dev>
2023-05-23 21:33:31 +0200

    add image picker and code for live image preview

--- app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
+++ app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
@@ -1,70 +1,228 @@
 package at.jku.students.multimediasystemtextrecognition
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asAndroidBitmap
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+const val LOG_TAG = "MMS"
 
 class MainActivity : ComponentActivity() {
+
+    var permissionGranted by mutableStateOf(false)
+
+    private val requestCameraPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Log.i(LOG_TAG, "Permission granted")
+            permissionGranted = true
+        } else {
+            Log.i(LOG_TAG, "Permission denied")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Displaying edge-to-edge
         //WindowCompat.setDecorFitsSystemWindows(window, false)
 
+
+//        requestCameraPermission()
         setContent {
             MaterialTheme {
                 Welcome()
+//                if (permissionGranted) {
+//                    Log.i(LOG_TAG, "Showing camera")
+//                    CameraView()
+//                }
             }
 
+        }
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i(LOG_TAG, "Permission previously granted")
+                permissionGranted = true
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.CAMERA
+            ) -> Log.i(LOG_TAG, "Show camera permissions dialog")
+
+            else -> requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
     }
 }
 
 @Composable
-@Preview
 fun Welcome() {
+
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
     var detectedText by remember { mutableStateOf("") }
 
-    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    if (bitmap.value != null) {
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
-    val img = ImageBitmap.imageResource(id = R.drawable.test)
+        val inp = InputImage.fromBitmap(bitmap.value!!, 0)
 
-    val inp = InputImage.fromBitmap(img.asAndroidBitmap(), 0)
+        recognizer.process(inp)
+            .addOnSuccessListener {
+                detectedText = it.text
+            }
+            .addOnFailureListener{
+                Log.e(LOG_TAG, it.toString())
+                detectedText = it.toString()
+            }
+    }
 
-    recognizer.process(inp)
-        .addOnSuccessListener {
-            detectedText = it.text
-        }
-        .addOnFailureListener{
-            print(it.message)
-        }
+
 
     Column {
         Text(
-            text = "Hello World! $detectedText",
+            text = "Select an image!",
             color = Color.Black
         )
-        Image(
-            painter = painterResource(R.drawable.test),
-            contentDescription = ""
+        ImagePicker(bitmap = bitmap)
+        if (bitmap.value != null) {
+            Box {
+                Image(
+                    bitmap = bitmap.value!!.asImageBitmap(),
+                    contentDescription = ""
+                )
+            }
+            Text(
+                text = detectedText,
+                color = Color.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun ImagePicker(bitmap: MutableState<Bitmap?>) {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(contract =
+    ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri
+    }
+    Column() {
+        Button(onClick = {
+            launcher.launch("image/*")
+        }) {
+            Text(text = "Pick image")
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        imageUri?.let {
+            if (Build.VERSION.SDK_INT < 28) {
+                bitmap.value = MediaStore.Images
+                    .Media.getBitmap(context.contentResolver,it)
+
+            } else {
+                val source = ImageDecoder
+                    .createSource(context.contentResolver,it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun CameraView() {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val ctx = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val preview = Preview.Builder().build()
+    val previewView = remember { PreviewView(ctx) }
+    val cameraSelector = CameraSelector.Builder()
+        .requireLensFacing(lensFacing)
+        .build()
+
+    LaunchedEffect(lensFacing) {
+
+        val cameraProvider = ctx.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            cameraSelector,
+            preview
         )
+
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    Box(contentAlignment = Alignment.BottomCenter) {
+        AndroidView({ previewView })
+    }
+
+}
+
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+        cameraProvider.addListener({
+            continuation.resume(cameraProvider.get())
+        }, ContextCompat.getMainExecutor(this))
     }
 }
