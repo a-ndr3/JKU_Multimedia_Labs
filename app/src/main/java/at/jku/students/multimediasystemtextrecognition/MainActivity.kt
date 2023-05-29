be0e666e07041ce1e60f207e5f9320838454ddba
tschan <jan.rendel@gmail.com>
2023-05-29 15:25:22 +0200

    Text recognition now updates after filtering the image

--- app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
+++ app/src/main/java/at/jku/students/multimediasystemtextrecognition/MainActivity.kt
@@ -1,7 +1,6 @@
 package at.jku.students.multimediasystemtextrecognition
 
 import android.content.Intent
-import android.content.res.Resources.NotFoundException
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
@@ -49,20 +48,7 @@ class MainActivity : AppCompatActivity() {
             image!!.setImageURI(imageUri)
             originalImage = (image!!.drawable as BitmapDrawable).bitmap
 
-            // applying google ml kit
-            val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-            val inp = InputImage.fromBitmap((image!!.drawable as BitmapDrawable).bitmap, 0)
-
-            // textView reference to update
-            val textView: TextView = findViewById(R.id.txt)
-
-            recognizer.process(inp)
-                .addOnSuccessListener {
-                    textView.text = it.text
-                }
-                .addOnFailureListener{
-                    textView.text = it.toString()
-                }
+            recognizeText()
         }
     }
 
@@ -77,6 +63,7 @@ class MainActivity : AppCompatActivity() {
     // TODO user input option to set filter strength
     // TODO implement generic way to apply a filter. Right now it looks disgusting
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var filteredBitmap : Bitmap? = null
         if (image == null) {
             // TODO alert the user if no image is selected
             Log.i("INFO", "No image selected")
@@ -85,61 +72,61 @@ class MainActivity : AppCompatActivity() {
 
             when (item.itemId) {
                 R.id.brightHSV -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.BRIGHTNESS_HSV).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.bw -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.BLACK_WHITE).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.avg -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.AVERAGING).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.med -> {
-                    val filteredBitmap =
-                        filter.createFilter(FilterTypes.MEDIAN).apply(originalImage!!, 10)
+                    filteredBitmap =
+                        filter.createFilter(FilterTypes.MEDIAN).apply(originalImage!!, 1)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.hueHSV -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.HUE_HSV).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.satHSV -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.SATURATION_HSV).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.con -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.CONTRAST).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.edgeC -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.EDGE_COLORING).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.bin -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.BINARY).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
 
                 R.id.sha -> {
-                    val filteredBitmap =
+                    filteredBitmap =
                         filter.createFilter(FilterTypes.SHARPEN).apply(originalImage!!, 10)
                     image!!.setImageBitmap(filteredBitmap)
                 }
@@ -148,6 +135,24 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+        recognizeText()
         return true
+    }
+
+    fun recognizeText() {
+        // applying google ml kit
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        val inp = InputImage.fromBitmap((image!!.drawable as BitmapDrawable).bitmap, 0)
+
+        // textView reference to update
+        val textView: TextView = findViewById(R.id.txt)
+
+        recognizer.process(inp)
+            .addOnSuccessListener {
+                textView.text = it.text
+            }
+            .addOnFailureListener{
+                textView.text = it.toString()
+            }
     }
 }
